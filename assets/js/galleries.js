a2fc08da904b2c571e54214b22ace3acd3454f7c
Minhaj-Hubar <minhajul@swordfishtech.co.uk>
2025-07-22 12:06:18 +0600

    event page fixed

---
+++ assets/js/galleries.js
@@ -0,0 +1,20 @@
+const galleries = {
+  "tree-planting": {
+    title: "Tree Planting Drive 2024",
+    description: "We planted 500 trees along the main roads of Kashipur, creating a greener and healthier environment for our community.",
+    images: [
+      "assets/images/tree-planting.jpg",
+      "assets/images/yo.jpg",
+      "assets/images/yo2.jpg"
+    ]
+  },
+  "education-support": {
+    title: "Education Support 2023",
+    description: "Supported 150 students with educational materials and scholarships, helping them achieve their academic goals.",
+    images: [
+      "assets/images/student.jpg",
+      "assets/images/burger.jpg",
+      "assets/images/kawsar.jpg"
+    ]
+  }
+}; 
