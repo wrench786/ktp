dc48546f9fe2dadcbe3294504c34aee651cb3745
Minhaj-Hubar <minhajul@swordfishtech.co.uk>
2025-08-05 20:42:46 +0600

    added cloud image.

--- assets/js/galleries.js
+++ assets/js/galleries.js
@@ -1,13 +1,29 @@
 // Gallery data for event-gallery.html
 // This object contains all the gallery information for different events
 const galleries = {
-  "tree-planting": {
-    title: "Tree Planting Drive 2024",
+  "ssc-ceremony-2025": {
+    title: "এসএসসি-তে কৃতি শিক্ষার্থীদের সংবর্ধনা ও শুভেচ্ছা অনুষ্ঠান - ২০২৫",
+    description: "২০২৫ সালের এসএসসি পরীক্ষায় কৃতিত্বপূর্ণ ফল অর্জনকারী শিক্ষার্থীদের সম্মাননা জানাতে আয়োজিত হয়েছিল কৃতি শিক্ষার্থীদের সংবর্ধনা ও শুভেচ্ছা অনুষ্ঠান । এই আয়োজনে কৃতি শিক্ষার্থীদের হাতে তুলে দেওয়া হয়েছিল সম্মাননা স্মারক, ছিল শিক্ষকদের শুভেচ্ছা বাণী, প্রেরণাদায়ক বক্তব্য ও মনোজ্ঞ সাংস্কৃতিক পরিবেশনা। এটি ছিল শুধু একটি অনুষ্ঠান নয়, বরং ভবিষ্যৎ গড়ার পথচলায় এক অনন্য প্রেরণা ।",
+    images: [
+      "https://i.postimg.cc/JnVwbP6g/1.jpg",
+      "https://i.postimg.cc/qMm9mkD2/3.jpg",
+      "https://i.postimg.cc/8czZWvS9/4.jpg",
+      "https://i.postimg.cc/ZRZ7bgDT/5.jpg",
+      "https://i.postimg.cc/fbYqhZC2/6.jpg",
+      "https://i.postimg.cc/RFRPNRjw/7.jpg",
+      "https://i.postimg.cc/pLwc7yf5/8.jpg"
+    ]
+  },
+
+  "tree-planting-2025": {
+    title: "বৃক্ষরোপণ কর্মসূচি ২০২৫",
     description: "We planted 500 trees along the main roads of Kashipur, creating a greener and healthier environment for our community.",
     images: [
-      "assets/images/tree-planting.jpg",
-      "assets/images/yo.jpg",
-      "assets/images/yo2.jpg"
+      "https://i.postimg.cc/tRD29szV/6.jpg",
+      "https://i.postimg.cc/43tWGw8J/4.jpg",
+      "https://i.postimg.cc/HWQ1z9d1/1.jpg",
+      "https://i.postimg.cc/wBSnc7rB/2.jpg",
+      "https://i.postimg.cc/Pxj25Zc1/5.jpg"
     ]
   },
   "education-support": {
